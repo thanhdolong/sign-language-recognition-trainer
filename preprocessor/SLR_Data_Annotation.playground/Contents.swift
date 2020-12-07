6791c7503d7a46c0035a2786f1e7675aeef1281e
Matyáš Boháček <matyas.bohacek@matsworld.io>
2020-12-07 17:45:21 +0100

    Add new demonstration code

--- preprocessor/SLR_Data_Annotation.playground/Contents.swift
+++ preprocessor/SLR_Data_Annotation.playground/Contents.swift
@@ -7,16 +7,18 @@ import CreateML
 // Demonstration of the annotations script. The video is analyzed for all of the relevant
 // key landmarks and this data is further structured into a CSV file.
 //
-// TODO: Implement a structure for "soaking" entire structured directories with automatic
-// labelling
 // TODO: Update the Vision keys to new, non-deprecated values
-// TODO: Add information on FPS and video size into the CSV files
 //
 
 
-// Path to desktop
+// Path to dataset
 let fileManager = FileManager.default
-var desktopPath = ((NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true) as [String]).first ?? "")
+var datasetPath = ((NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true) as [String]).first ?? "").appending("/dictio_cislovky")
+
+
+/*
+// OPTION 1: PROCESSING ONE BY ONE
+
 
 // Experimental label and URL to one of the videos
 let labels = ["test"]
@@ -33,6 +35,22 @@ do {
     let dataTable = try OutputDataStructuringManager.combineData(labels: labels, visionAnalyses: [analysisManager!])
     
     // Save it to Desktop folder in CSV
-    try dataTable.writeCSV(to: URL(fileURLWithPath: desktopPath).appendingPathComponent("test.csv"))
+    try dataTable.writeCSV(to: URL(fileURLWithPath: datasetPath).appendingPathComponent("individual_test.csv"))
 } catch {}
+*/
 
+// OPTION 2: PROCESSING THE ENTIRE DATASET
+
+
+// Create a dataset manager to process the entire dataset
+let datasetManager = DatasetManager(directoryPath: datasetPath, fps: 3)
+
+do {
+    // Structure the data into a MLDataTable
+    let dataTable = try datasetManager!.generateMLTable()
+    
+    // Save it to Desktop folder in CSV
+    try dataTable.writeCSV(to: URL(fileURLWithPath: datasetPath).appendingPathComponent("dataset_test.csv"))
+} catch {
+    print(error)
+}
